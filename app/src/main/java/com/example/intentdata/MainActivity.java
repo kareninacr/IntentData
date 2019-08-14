748685265728f68856f3f5a73079b59e9ef723a6
Karenina <kareninacasandrar76@gmail.com>
2019-08-14 15:17:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/intentdata/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.intentdata;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText name = (EditText) findViewById(R.id.textData);
+        Button button = (Button) findViewById(R.id.submit);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                i.putExtra("name", name.getText().toString());
+                startActivity(i);
+            }
+        });
+    }
+}
